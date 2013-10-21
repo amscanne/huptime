fd30c2d293c13f3fd5a80fa6aa111b1faea1e2c1
Adin Scannell <adin@scannell.ca>
2013-10-20 23:16:30 -0400

    Adding license information.

--- src/funcs.h
+++ src/funcs.h
@@ -1,5 +1,22 @@
 /*
  * funcs.h
+ *
+ * Copyright 2013 Adin Scannell <adin@scannell.ca>, all rights reserved.
+ *
+ * This file is part of Huptime.
+ *
+ * Huptime is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Huptime is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Huptime.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _FUNCS_H_
